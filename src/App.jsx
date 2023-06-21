cf737af4840c76617dad4e6811eb15818dd8cb1f
Bence <m.bence211@gmail.com>
2023-06-21 22:28:22 +0200

    Navbar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./components/Navbar";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [selectedPage, setSelectedPage] = useState("home");
+	const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
-	return <></>;
+	return (
+		<div className="app bg-deep-blue ">
+			<Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+		</div>
+	);
 }
 
 export default App;
